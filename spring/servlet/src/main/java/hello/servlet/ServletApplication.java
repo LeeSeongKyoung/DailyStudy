78aa7f75df5d1c5c34989f801235fb5c4ff6c337
LeeSeongKyong <llee_ss@naver.com>
2022-01-27 15:13:22 +0900

    update servlet

--- spring/servlet/src/main/java/hello/servlet/ServletApplication.java
+++ spring/servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,10 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan // 서블릿 자동 등록
 @SpringBootApplication
@@ -11,5 +15,13 @@ public class ServletApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
+
+/*
+	스프링 부트가 알아서 해줌
+	@Bean
+	ViewResolver internalResourceViewResolver(){ // 내부에서 자원을 전달?
+		return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp")
+	}
+*/
 
 }
