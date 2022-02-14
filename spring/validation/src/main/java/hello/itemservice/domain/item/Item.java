2627dea22bcd7722a5fc28ae6e648881442f7943
LeeSeongKyong <llee_ss@naver.com>
2022-02-14 09:46:15 +0900

    update files

--- spring/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ spring/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -9,21 +9,20 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
-//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10,000원 넘게 입력해주세요.")
-// -> 실제 사용해보면 제약이 많고 복잡하다
 public class Item {
 
+    @NotNull(groups = UpdateCheck.class) // 수정 요구사항 추가
     private Long id;
 
-    @NotBlank(message = "공백x")
+    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
 
-    @NotNull
-    @Range(min= 1_000, max = 1_000_000)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Range(min= 1_000, max = 1_000_000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
 
-    @NotNull
-    @Max(9999)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Max(value = 9999, groups = {SaveCheck.class})
     private Integer quantity;
 
     public Item() {
